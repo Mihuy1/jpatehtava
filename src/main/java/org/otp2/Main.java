532772407f435ad9f150f14e891578be26344b93
Mihuy1 <patrik.hyytiainen@protonmail.com>
2025-02-13 17:20:54 +0200

    chore: Remove unnecessary comments

--- src/main/java/org/otp2/Main.java
+++ src/main/java/org/otp2/Main.java
@@ -5,15 +5,12 @@ public class Main {
         StudentDAO studentDAO = new StudentDAO();
         TimeSpentDAO timeSpentDAO = new TimeSpentDAO();
 
-        // Create a new student
         Student student = new Student("Patrik Hyytiainen", "patrikhy@metropolia.fi");
         studentDAO.addStudent(student);
 
-        // Create a time record for the student
         TimeSpent timeSpent = new TimeSpent(3, 4, 2, student);
         timeSpentDAO.addTimeSpent(timeSpent);
 
-        // Retrieve student and check their time spent records
         Student retrievedStudent = studentDAO.findStudent(student);
         System.out.println(retrievedStudent);
     }
